8edf040d12e56e4cd2e8d7a68de5ec7f3f7c2bb8
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-31 18:42:46 +0530

    room_endpoint

--- apps/server/core/http/router.go
+++ apps/server/core/http/router.go
@@ -9,6 +9,7 @@ func NewRouter(hub *ws.Hub) http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", Health)
+	mux.HandleFunc("/room", Room)
 
 	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		ws.ServeWS(hub, w, r)
