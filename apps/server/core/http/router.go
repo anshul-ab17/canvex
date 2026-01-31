6e583f22e591d2c4a86559b9a594e25c8f63391e
Anshul Bharat <anshul.ab17x@gmail.com>
2026-01-31 23:43:51 +0530

    separating - server

--- apps/server/core/http/router.go
+++ apps/server/core/http/router.go
@@ -1,21 +1,14 @@
 package http
 
-import (
-	"net/http"
+import "net/http"
 
-	"server/core/ws"
-)
-
-func NewRouter(hub *ws.Hub) http.Handler {
+func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", Health)
 	mux.HandleFunc("/rooms", Rooms)
 	mux.HandleFunc("/rooms/", GetRoom)
 
-	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		ws.ServeWS(hub, w, r)
-	})
 
 	var handler http.Handler = mux
 	handler = Logger(handler)
--- apps/server/http-server/main.go
+++ apps/server/http-server/main.go
@@ -5,20 +5,9 @@ import (
 	"net/http"
 
 	httpLayer "server/core/http"
-	"server/core/ws"
 )
-func Logger(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.URL.Path)
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	hub := ws.NewHub()
-	go hub.Run()
-
-	router := httpLayer.NewRouter(hub)
+	router := httpLayer.NewRouter()
 
 	log.Println("Canvex running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
